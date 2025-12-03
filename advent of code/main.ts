54c8858619dc7e8f5b462a8e496c6af1aeeecad0
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-03 07:09:30 +0200

    Day 3, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -7,31 +7,20 @@ let inputString = (await Deno.readTextFile(filePath)) as string;
 
 console.log(inputString);
 
-const moves = inputString.replace(/\r\n/g, "").split(`,`);
+const moves = inputString.split(`\r\n`);
 console.log(moves);
 let count = 0;
 for (const [index, move] of moves.entries()) {
-  const [first, second] = move.split("-");
-  for (let i = +first; i <= +second; i++) {
-    const text = `${i}`;
-    // console.log(text);
-    for (let j = 0; j <= text.length - 1; j++) {
-      // console.log(`j: ${j}`);
-      let subString = "";
-      for (let k = 0; k < j; k++) {
-        // console.log(`k: ${k}`);
-        subString += text[k];
-      }
-      // console.log(subString);
-      const repetitions = text.length / j;
-      if (repetitions % 1 === 0) {
-        if (text === subString.repeat(repetitions)) {
-          console.log(`Found: ${text}, ${subString}`);
-          count += i;
-          break;
-        }
+  let best = "00";
+  for (let i = 0; i < move.length; i++) {
+    for (let j = i + 1; j < move.length; j++) {
+      if (move[i] + move[j] > best) {
+        best = move[i] + move[j];
       }
     }
   }
+  console.log(best);
+  // console.log(count);
+  count += +best;
 }
 console.log(count);
