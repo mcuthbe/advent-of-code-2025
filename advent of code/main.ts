3256c1f1d6214501c866b2b0ecbf069a187deae8
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-03 09:12:34 +0200

    Day 3, part 2

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -1,26 +1,37 @@
-import { memoise } from "./helpers.ts";
+import { memoise, permutator } from "./helpers.ts";
 
 // Define the path to the text file
 const filePath = "./input.txt";
 
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
-console.log(inputString);
-
 const moves = inputString.split(`\r\n`);
-console.log(moves);
-let count = 0;
-for (const [index, move] of moves.entries()) {
-  let best = "00";
-  for (let i = 0; i < move.length; i++) {
-    for (let j = i + 1; j < move.length; j++) {
-      if (move[i] + move[j] > best) {
-        best = move[i] + move[j];
-      }
+let start = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let options: number[][] = [start];
+
+const decrease = (current: string) => {
+  while (current.length > 12) {
+    let options: string[] = [];
+    for (let i = 0; i < current.length; i++) {
+      const option = current.slice(0, i) + current.slice(i + 1);
+      // console.log(i);
+      // console.log(current.slice(0, i));
+      // console.log(current.slice(i + 1));
+      // console.log(option);
+      options.push(option);
     }
+    current = options.sort((a, b) => +b - +a)[0];
+    // console.log(current);
   }
-  console.log(best);
-  // console.log(count);
-  count += +best;
+  return current;
+};
+
+let count = 0;
+
+for (const [index, move] of moves.entries()) {
+  const decreased = decrease(move);
+  console.log(decreased);
+  count += +decreased;
+  // console.log(move.slice(1));
 }
 console.log(count);
