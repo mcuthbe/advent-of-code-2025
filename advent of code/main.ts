ef7a3a1878541f309a38890533f5bb6fdbdea244
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-04 07:19:31 +0200

    Day 4, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -6,32 +6,37 @@ const filePath = "./input.txt";
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
 const moves = inputString.split(`\r\n`);
-let start = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let options: number[][] = [start];
-
-const decrease = (current: string) => {
-  while (current.length > 12) {
-    let options: string[] = [];
-    for (let i = 0; i < current.length; i++) {
-      const option = current.slice(0, i) + current.slice(i + 1);
-      // console.log(i);
-      // console.log(current.slice(0, i));
-      // console.log(current.slice(i + 1));
-      // console.log(option);
-      options.push(option);
-    }
-    current = options.sort((a, b) => +b - +a)[0];
-    // console.log(current);
-  }
-  return current;
-};
-
 let count = 0;
 
-for (const [index, move] of moves.entries()) {
-  const decreased = decrease(move);
-  console.log(decreased);
-  count += +decreased;
-  // console.log(move.slice(1));
+const grid: Record<string, string> = {};
+
+for (const [row, move] of moves.entries()) {
+  for (const [col, char] of move.split("").entries()) {
+    grid[`${row},${col}`] = char;
+  }
 }
+
+for (const key in grid) {
+  const [row, col] = key.split(",").map(Number);
+  let rocks = 0;
+  if (grid[key] === ".") {
+    continue;
+  }
+  console.log("Checking " + key, grid[key]);
+  for (let i = row - 1; i <= row + 1; i++) {
+    for (let j = col - 1; j <= col + 1; j++) {
+      const newKey = `${i},${j}`;
+      if (key === "1,4") console.log(`${newKey},${grid[newKey]}`);
+      if (grid[newKey] === "@" && newKey != key) {
+        rocks++;
+      }
+    }
+  }
+  if (rocks < 4) {
+    console.log("Rocks: " + rocks);
+    count++;
+    // grid[key] = "X";
+  }
+}
+// console.log(grid);
 console.log(count);
