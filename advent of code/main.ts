d25358c45ac16bcda92cfcc35cb72839cef656e0
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-05 08:43:42 +0200

    Day 5, part 2

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -6,27 +6,63 @@ const filePath = "./input.txt";
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
 const moves = inputString.split(`\r\n`);
+type Range = { min: number; max: number };
 const ranges =
   inputString.match(/.*-.*/gm)?.map((range) => {
     const [min, max] = range.split("-").map(Number);
     return { min, max };
   }) || [];
-const ids = inputString.match(/^\d+$/gm)?.map(Number) || [];
 
 let count = 0;
 
-for (const id of ids) {
-  let found = false;
-  console.log(id);
-  for (const range of ranges) {
-    if (id >= range.min && id <= range.max) {
-      found = true;
-      break;
+let validRanges: Range[] = [];
+
+const compareRanges = (a: Range, b: Range) => {
+  // console.log(`Comparing: ${JSON.stringify(a)}, ${JSON.stringify(b)}`);
+  //fully inside, skip
+  if (a.min >= b.min && a.max <= b.max) {
+    return [b];
+  }
+  //fully outside, separate range
+  if (a.max < b.min || a.min > b.max) {
+    // console.log("No replacement");
+    return [a, b];
+  }
+  //fully bigger, swap
+  if (a.min <= b.min && a.max >= b.max) {
+    return [a];
+  }
+  //left overlapping
+  if (a.min < b.min && a.max <= b.max) {
+    return [{ min: a.min, max: b.max }];
+  }
+  //right overlapping
+  if (a.min >= b.min && a.max > b.max) {
+    return [{ min: b.min, max: a.max }];
+  }
+  console.log("No replacement any");
+};
+
+let currentRanges = ranges;
+for (let i = 0; i < currentRanges.length; i++) {
+  loop2: for (let j = i + 1; j < currentRanges.length; j++) {
+    const comparedRanges = compareRanges(currentRanges[i], currentRanges[j]);
+
+    if (comparedRanges && comparedRanges.length !== 2) {
+      console.log("Replacement:" + JSON.stringify(comparedRanges));
+      currentRanges.splice(j, 1);
+      currentRanges.splice(i, 1);
+      currentRanges.push(...comparedRanges);
+      i = -1;
+      break loop2;
     }
   }
-  if (found) {
-    count++;
-  }
+  console.log(i);
+  // console.log(currentRanges);
+}
+
+for (const range of currentRanges) {
+  count += range.max - range.min + 1;
 }
 
 console.log(count);
