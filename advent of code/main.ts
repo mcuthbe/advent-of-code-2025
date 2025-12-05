6c389fdf8b26d777a651d0edca323515efc0edc3
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-05 07:09:28 +0200

    Day 5, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -6,51 +6,27 @@ const filePath = "./input.txt";
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
 const moves = inputString.split(`\r\n`);
-let finalCount = 0;
+const ranges =
+  inputString.match(/.*-.*/gm)?.map((range) => {
+    const [min, max] = range.split("-").map(Number);
+    return { min, max };
+  }) || [];
+const ids = inputString.match(/^\d+$/gm)?.map(Number) || [];
 
-const grid: Record<string, string> = {};
+let count = 0;
 
-for (const [row, move] of moves.entries()) {
-  for (const [col, char] of move.split("").entries()) {
-    grid[`${row},${col}`] = char;
-  }
-}
-let done = false;
-while (!done) {
-  console.log("Attempting");
-  let count = 0;
-
-  for (const key in grid) {
-    const [row, col] = key.split(",").map(Number);
-    let rocks = 0;
-    if (grid[key] === ".") {
-      continue;
-    }
-    // console.log("Checking " + key, grid[key]);
-    for (let i = row - 1; i <= row + 1; i++) {
-      for (let j = col - 1; j <= col + 1; j++) {
-        const newKey = `${i},${j}`;
-        if ((grid[newKey] === "@" || grid[newKey] === "X") && newKey != key) {
-          rocks++;
-        }
-      }
-    }
-    if (rocks < 4) {
-      // console.log("Rocks: " + rocks);
-      count++;
-      grid[key] = "X";
+for (const id of ids) {
+  let found = false;
+  console.log(id);
+  for (const range of ranges) {
+    if (id >= range.min && id <= range.max) {
+      found = true;
+      break;
     }
   }
-  for (const key in grid) {
-    if (grid[key] === "X") {
-      grid[key] = ".";
-    }
-  }
-  finalCount += count;
-  console.log(count);
-  if (count === 0) {
-    done = true;
+  if (found) {
+    count++;
   }
 }
 
-console.log(finalCount);
+console.log(count);
