e0aa4b2bc6ec7717096b98c5cf55f6ef77c3bb85
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-06 07:16:20 +0200

    Day 6, part 2

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -6,63 +6,36 @@ const filePath = "./input.txt";
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
 const moves = inputString.split(`\r\n`);
-type Range = { min: number; max: number };
-const ranges =
-  inputString.match(/.*-.*/gm)?.map((range) => {
-    const [min, max] = range.split("-").map(Number);
-    return { min, max };
-  }) || [];
 
+type Entry = { operation: "+" | "*"; values: number[] };
+const dict: Record<number, Entry> = {};
+
+for (const move of moves) {
+  const strings = move.split(" ").filter((x) => x.length > 0);
+  console.log(strings);
+  for (const [index, str] of strings.entries()) {
+    dict[index] = dict[index] || { operation: "+", values: [] };
+    if (str === "+" || str === "*") {
+      dict[index].operation = str;
+    }
+
+    const num = Number(str);
+    if (isNaN(num)) continue;
+    dict[index].values.push(num);
+  }
+}
 let count = 0;
-
-let validRanges: Range[] = [];
-
-const compareRanges = (a: Range, b: Range) => {
-  // console.log(`Comparing: ${JSON.stringify(a)}, ${JSON.stringify(b)}`);
-  //fully inside, skip
-  if (a.min >= b.min && a.max <= b.max) {
-    return [b];
-  }
-  //fully outside, separate range
-  if (a.max < b.min || a.min > b.max) {
-    // console.log("No replacement");
-    return [a, b];
-  }
-  //fully bigger, swap
-  if (a.min <= b.min && a.max >= b.max) {
-    return [a];
-  }
-  //left overlapping
-  if (a.min < b.min && a.max <= b.max) {
-    return [{ min: a.min, max: b.max }];
-  }
-  //right overlapping
-  if (a.min >= b.min && a.max > b.max) {
-    return [{ min: b.min, max: a.max }];
-  }
-  console.log("No replacement any");
-};
-
-let currentRanges = ranges;
-for (let i = 0; i < currentRanges.length; i++) {
-  loop2: for (let j = i + 1; j < currentRanges.length; j++) {
-    const comparedRanges = compareRanges(currentRanges[i], currentRanges[j]);
-
-    if (comparedRanges && comparedRanges.length !== 2) {
-      console.log("Replacement:" + JSON.stringify(comparedRanges));
-      currentRanges.splice(j, 1);
-      currentRanges.splice(i, 1);
-      currentRanges.push(...comparedRanges);
-      i = -1;
-      break loop2;
+for (const entry of Object.values(dict)) {
+  let total = entry.values[0];
+  for (const val of entry.values.slice(1)) {
+    if (entry.operation === "+") {
+      total += val;
+    } else {
+      total *= val;
     }
   }
-  console.log(i);
-  // console.log(currentRanges);
+  console.log(JSON.stringify(entry));
+  console.log(total);
+  count += total;
 }
-
-for (const range of currentRanges) {
-  count += range.max - range.min + 1;
-}
-
 console.log(count);
