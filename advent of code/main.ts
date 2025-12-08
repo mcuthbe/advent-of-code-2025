fb2b94ba84e2331c750e666efd5401dab93a9867
Mitchell Cuthbertson <mitchell.cuthbertson@co-flo.co.za>
2025-12-08 16:56:34 +0200

    Day 8, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -5,78 +5,156 @@ const filePath = "./input.txt";
 
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
-const moves = inputString.split(`\r\n`);
+const lines = inputString.split(`\r\n`);
 
-type Entry = { operation: "+" | "*"; values: number[] };
+type Position = {
+  x: number;
+  y: number;
+  z: number;
+};
 
-let count = 0;
+const calculateDistance = (a: Position, b: Position) => {
+  return Math.abs(
+    Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2 + (b.z - a.z) ** 2)
+  );
+};
 
-type Coord = { x: number; y: number };
-type Dupes = { coord: Coord; count: number };
+const pointsEquals = (a: Position, b: Position) => {
+  return a.x === b.x && a.y === b.y && a.z === b.z;
+};
 
-type Map = Record<number, Record<number, string>>;
+let connections: { first: Position; second: Position; distance: number }[] = [];
 
-let map: Map = {};
-let startPos: Coord = { x: 0, y: 0 };
-let checked: Record<`${number},${number}`, number> = {};
+for (let i = 0; i < lines.length; i++) {
+  const [x, y, z] = lines[i].split(",").map((n) => +n);
+  const position = { x, y, z };
+  for (let j = i + 1; j < lines.length; j++) {
+    const [x2, y2, z2] = lines[j].split(",").map((n) => +n);
+    const position2 = { x: x2, y: y2, z: z2 };
+    const distance = calculateDistance(position, position2);
 
-let locations: Dupes[] = [];
-
-inputString.split("\r\n").forEach((line, lineIndex) => {
-  line.split("").forEach((char, charIndex) => {
-    map[lineIndex] ??= {};
-    if (char === "S") {
-      startPos = { x: charIndex, y: lineIndex };
-      locations.push({ coord: startPos, count: 1 });
-    }
-    map[lineIndex][charIndex] = char;
-  });
-});
-// console.log(map);
-// console.log(locations);
-// console.log(map[Array.from(locations)[0].y][Array.from(locations)[0].x]);
-let done = false;
-while (locations.length > 0 && !done) {
-  let newLocations: Dupes[] = [];
-  let deduped: Dupes[] = [];
-  for (const location of locations) {
-    const existing = deduped.find(
-      (loc) =>
-        loc.coord.x === location.coord.x && loc.coord.y === location.coord.y
-    );
-    if (existing) {
-      existing.count += location.count;
-    } else {
-      deduped.push(location);
-    }
+    connections.push({ first: position, second: position2, distance });
   }
-  console.log(deduped);
-  for (const location of deduped) {
-    const newY = location.coord.y + 1;
-    if (map[newY]?.[location.coord.x] === ".") {
-      newLocations.push({
-        coord: { x: location.coord.x, y: newY },
-        count: location.count,
-      });
-    }
-    if (map[newY]?.[location.coord.x] === "^") {
-      newLocations.push({
-        coord: { x: location.coord.x + 1, y: newY },
-        count: location.count,
-      });
-      newLocations.push({
-        coord: { x: location.coord.x - 1, y: newY },
-        count: location.count,
-      });
-    }
-  }
-  if (!locations.every((location) => map[location.coord.y][location.coord.x])) {
-    done = true;
-  } else {
-    count = locations.reduce((acc, loc) => acc + loc.count, 0);
-  }
-  locations = newLocations;
-  // console.log(locations);
 }
 
-console.log(count);
+let circuits: Position[][] = [];
+
+connections
+  .toSorted((a, b) => a.distance - b.distance)
+  .filter((_, index) => index < 1000)
+  .forEach(({ first, second }) => {
+    let done = false;
+    if (first.x === 19137 || second.x === 19137) {
+      console.log("starting", first, second);
+      console.log(
+        circuits.filter((circuit) =>
+          circuit.find(
+            (pos) => pointsEquals(pos, first!) || pointsEquals(pos, second!)
+          )
+        )
+      );
+    }
+
+    if (
+      circuits.filter((circuit) =>
+        circuit.find(
+          (pos) => pointsEquals(pos, first!) || pointsEquals(pos, second!)
+        )
+      ).length > 1
+    ) {
+      const firstCircuitIndex = circuits.findIndex(
+        (circuit) => !!circuit.find((pos) => pointsEquals(pos, first!))
+      );
+      const secondCircuitIndex = circuits.findIndex(
+        (circuit) => !!circuit.find((pos) => pointsEquals(pos, second!))
+      );
+      if (firstCircuitIndex === -1 || secondCircuitIndex === -1) {
+        console.log(
+          "Circuits",
+          circuits.filter((circuit) =>
+            circuit.find(
+              (pos) => pointsEquals(pos, first!) || pointsEquals(pos, second!)
+            )
+          )
+        );
+        return;
+      }
+      if (
+        firstCircuitIndex !== -1 &&
+        secondCircuitIndex !== -1 &&
+        firstCircuitIndex !== secondCircuitIndex
+      ) {
+        const newCircuit = new Set([
+          ...circuits[firstCircuitIndex],
+          ...circuits[secondCircuitIndex],
+        ]);
+        if (firstCircuitIndex > secondCircuitIndex) {
+          circuits.splice(firstCircuitIndex, 1);
+          circuits.splice(secondCircuitIndex, 1);
+        } else {
+          circuits.splice(secondCircuitIndex, 1);
+          circuits.splice(firstCircuitIndex, 1);
+        }
+        circuits.push(Array.from(newCircuit));
+        done = true;
+        if (first.x === 19137 || second.x === 19137) {
+          console.log("Merging circuits");
+        }
+        // if (Array.from(newCircuit).some((pos) => pos.x === 19137)) {
+        //   console.log("Merged from elsewhere", first, second);
+        // }
+      }
+    }
+    if (done) return;
+    circuits.forEach((circuit, index) => {
+      const has1 = circuit.find((pos) => pointsEquals(pos, first!));
+      const has2 = circuit.find((pos) => pointsEquals(pos, second!));
+      if (has1 && has2) {
+        done = true;
+        if (first.x === 19137 || second.x === 19137) {
+          console.log("Has1 && has2");
+        }
+        return;
+      }
+      if (has1) {
+        done = true;
+        if (first.x === 19137 || second.x === 19137) {
+          console.log("Has1");
+        }
+        circuit.push(second!);
+        return;
+      }
+      if (has2) {
+        if (first.x === 19137 || second.x === 19137) {
+          console.log("Has2");
+        }
+        done = true;
+        circuit.push(first!);
+        return;
+      }
+    });
+    if (!done) {
+      if (first.x === 19137 || second.x === 19137) {
+        console.log("New circuit");
+      }
+      circuits.push([first!, second!]);
+    }
+    if (first.x === 19137 || second.x === 19137) {
+      console.log("Connected", first, second);
+      console.log(
+        circuits.filter((circuit) =>
+          circuit.find(
+            (pos) => pointsEquals(pos, first!) || pointsEquals(pos, second!)
+          )
+        )
+      );
+    }
+  });
+
+let finalCount = 1;
+circuits = circuits.toSorted((a, b) => b.length - a.length);
+for (let i = 0; i < 3; i++) {
+  console.log(circuits[i].length);
+  finalCount *= circuits[i].length;
+}
+console.log(finalCount);
